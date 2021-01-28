5d06f5471a8b4e299efd9b727ff89494bea4da90
Sean Naatz <naatz@madisoncollege.edu>
2021-01-28 15:34:46 -0600

    READY FOR GRADING - Week 2: Your First Script

---
+++ week2-script.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Prints "Sean Naat" to the screen
+Prints "I have learned two important things about Python in this
+        module:"
+Prints "1: It is very important to put the heading of
+        #!/usr/bin/env python3 at the beginning of the nano editor so that
+        Linux knows to use Python3 to interperate your script."
+Prints "2: I have also learned that using the three double quotation marks
+        allows for multi-line commenting in a script without Python
+        interpreting what is actually written there."
+"""
+print("Sean Naatz")
+print("I have learned two important things about Python in this " \
+        "module:")
+print("1: It is very important to put the heading of " \
+        "#!/usr/bin/env python3 at the beginning of the nano editor " \
+        "so that Linux knows to use Python3 to interperate your " \
+        "script.")
+print("2: I have also learned that using the three double " \
+        "quotation marks allows for multi-line commenting in " \
+        "a script without Python interpreting what is actually "\
+        "written there.")
