f0b4be90e9aa47a97d276822fc53ac33224c41d3
Sean Naatz <naatz@madisoncollege.edu>
2021-05-10 07:09:37 -0500

    Week 14:JSON - READY FOR REGRADING

--- week14-json.py
+++ week14-json.py
@@ -16,13 +16,16 @@ can enter arguments before running the script and what is displayed in the help 
 parser = argparse.ArgumentParser(description="This is my JSON Python script")
 parser.add_argument('-i','--ip', dest = 'myIP', metavar = '[an IPv4 address]', type=str, help='<required> Enter an IPv4 Address')
 
+args = parser.parse_args()
 #This if statement will automatically bring up the help menu if no arguments are input before the script is run
 if len(sys.argv) == 1:
     parser.print_help()
     exit(0)
+#This defines the URL that will be used in the requests.get command below, which uses {args.myIP} to get the argument value
+URL = f'http://ipinfo.io/{args.myIP}/json'
 
-#This line retrieves the json data from the manually entered IP Address represented by {sys.argv[2]} from the  WebAPI
-json_raw = requests.get(f'http://ipinfo.io/{sys.argv[2]}/json')
+#This line retrieves the json data from the manually entered IP Address represented by {args.myIP} from the  WebAPI
+json_raw = requests.get(URL)
 
 #This will convert the raw json byte stream into a readable dictionary
 myDict = json.loads(json_raw.text)
